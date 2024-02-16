612a3dfd6b359fc26efb308b7a41ecb3ad51b6a4
Xavier <collantes.xavier@gmail.com>
2024-02-16 08:51:44 +0000

    Add unittests
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- java-tutorial/src/main/java/com/example/Greeting.java
+++ java-tutorial/src/main/java/com/example/Greeting.java
@@ -1,7 +1,20 @@
 package com.example;
 
+import com.engflow.fancytext.FancyFormatter;
+
 public class Greeting {
+    /**
+     * Converts input string to mixed-case.
+     *
+     * @param text user input string.
+     * @return Mixed-case string.
+     */
+    public static String convert(String text) {
+        FancyFormatter formatter = new FancyFormatter().standard();
+        return formatter.format(text);
+    }
+
     public static void sayHi() {
-        System.out.println("Hi!");
+        System.out.println(convert("Hi!"));
     }
 }
---
+++ java-tutorial/test/main/java/com/example/GreetingTest.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import com.engflow.fancytext.FancyFormatter;
+
+public class GreetingTest {
+
+    @Test
+    public void formatText() {
+        HashMap<String, String> candidates = new HashMap<String, String>();
+        candidates.put(
+            "Never gonna give you up, never gonna let you down",
+            "nEvEr gOnNa gIvE YoU Up, NeVeR GoNnA LeT YoU DoWn");
+        candidates.put(
+            "N3v3r gonn@ run @round @nd de$ert y0u, N3v3r gonn@ make y0u cry",
+            "n3v3r gOnN@ rUn @RoUnD @nD De$eRt y0u, N3V3R GoNn@ MaKe y0u cRy");
+        candidates.put("", "");
+
+        for (Map.Entry<String,String> candidate: candidates.entrySet()) {
+            assertEquals(
+                new FancyFormatter().convert(candidate.getKey()),
+                candidate.getValue()
+            );
+        }
+    }
+
+    // @Test
+    // public void standardOut() {
+
+    // }
+}
